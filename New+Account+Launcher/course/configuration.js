3a6e9b8c08026d716b3f4bb9cc85cb47104805cd
joedonnelly73 <122378955+joedonnelly73@users.noreply.github.com>
2023-01-10 09:05:11 -0600

    Delete New+Account+Launcher directory

--- New+Account+Launcher/course/configuration.js
+++
@@ -1,5 +0,0 @@
-DispatchRoot = parent.ccLauncherDispatchConfig["DispatchRoot"];
-DispatchVersion = parent.ccLauncherDispatchConfig["DispatchVersion"];
-ContentURL = parent.ccLauncherDispatchConfig["ContentURL"];
-PreLaunchConfigurationURL = parent.ccLauncherDispatchConfig["PreLaunchConfigurationURL"];
-DebugMode = false;
